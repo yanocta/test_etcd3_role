7e42191cd6ca55c696410d28b2a1e9f7a8cebc39
rianocta97 <rianocta97@gmail.com>
2022-05-10 20:59:21 +0700

    testing role

--- main.go
+++ main.go
@@ -4,227 +4,94 @@ import (
 	"context"
 	"fmt"
 	"log"
-	"strconv"
-	"time"
 
-	clientv3 "go.etcd.io/etcd/client/v3"
+	"go.etcd.io/etcd/clientv3"
 )
-
-var (
-	dialTimeout    = 2 * time.Second
-	requestTimeout = 10 * time.Second
-)
-
-func GetSingleValueDemo(ctx context.Context, kv clientv3.KV) {
-	fmt.Println("*** GetSingleValueDemo()")
-	// Delete all keys
-	kv.Delete(ctx, "key", clientv3.WithPrefix())
-
-	// Insert a key value
-	pr, err := kv.Put(ctx, "key", "444")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	rev := pr.Header.Revision
-
-	fmt.Println("Revision:", rev)
-
-	gr, err := kv.Get(ctx, "key")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println("Value: ", string(gr.Kvs[0].Value), "Revision: ", gr.Header.Revision)
-
-	// Modify the value of an existing key (create new revision)
-	kv.Put(ctx, "key", "555")
-
-	gr, _ = kv.Get(ctx, "key")
-	fmt.Println("Value: ", string(gr.Kvs[0].Value), "Revision: ", gr.Header.Revision)
-
-	// Get the value of the previous revision
-	gr, _ = kv.Get(ctx, "key", clientv3.WithRev(rev))
-	fmt.Println("Value: ", string(gr.Kvs[0].Value), "Revision: ", gr.Header.Revision)
-}
-
-func GetMultipleValuesWithPaginationDemo(ctx context.Context, kv clientv3.KV) {
-	fmt.Println("*** GetMultipleValuesWithPaginationDemo()")
-	// Delete all keys
-	kv.Delete(ctx, "key", clientv3.WithPrefix())
-
-	// Insert 50 keys
-	for i := 0; i < 50; i++ {
-		k := fmt.Sprintf("key_%02d", i)
-		kv.Put(ctx, k, strconv.Itoa(i))
-	}
-
-	opts := []clientv3.OpOption{
-		clientv3.WithPrefix(),
-		clientv3.WithSort(clientv3.SortByKey, clientv3.SortAscend),
-		clientv3.WithLimit(10),
-	}
-
-	gr, err := kv.Get(ctx, "key", opts...)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println("--- First page ---")
-	for _, item := range gr.Kvs {
-		fmt.Println(string(item.Key), string(item.Value))
-	}
-
-	lastKey := string(gr.Kvs[len(gr.Kvs)-1].Key)
-
-	fmt.Println("--- Second page ---")
-	opts = append(opts, clientv3.WithFromKey())
-	gr, _ = kv.Get(ctx, lastKey, opts...)
-
-	// Skipping the first item, which the last item from from the previous Get
-	for _, item := range gr.Kvs[1:] {
-		fmt.Println(string(item.Key), string(item.Value))
-	}
-}
-
-func WatchDemo(ctx context.Context, cli *clientv3.Client, kv clientv3.KV) {
-	fmt.Println("*** WatchDemo()")
-	// Delete all keys
-	kv.Delete(ctx, "key", clientv3.WithPrefix())
-
-	stopChan := make(chan interface{})
-	go func() {
-		watchChan := cli.Watch(ctx, "key", clientv3.WithPrefix())
-		for {
-			select {
-			case result := <-watchChan:
-				for _, ev := range result.Events {
-					fmt.Printf("%s %q : %q\n", ev.Type, ev.Kv.Key, ev.Kv.Value)
-				}
-			case <-stopChan:
-				fmt.Println("Done watching.")
-				return
-			}
-		}
-	}()
-
-	// Insert some keys
-	for i := 0; i < 10; i++ {
-		k := fmt.Sprintf("key_%02d", i)
-		kv.Put(ctx, k, strconv.Itoa(i))
-	}
-
-	// Make sure watcher go routine has time to recive PUT events
-	time.Sleep(time.Second)
-
-	stopChan <- 1
-
-	// Insert some more keys (no one is watching)
-	for i := 10; i < 20; i++ {
-		k := fmt.Sprintf("key_%02d", i)
-		kv.Put(ctx, k, strconv.Itoa(i))
-	}
-}
-
-func LeaseDemo(ctx context.Context, cli *clientv3.Client, kv clientv3.KV) {
-	fmt.Println("*** LeaseDemo()")
-	// Delete all keys
-	kv.Delete(ctx, "key", clientv3.WithPrefix())
-
-	gr, _ := kv.Get(ctx, "key")
-	if len(gr.Kvs) == 0 {
-		fmt.Println("No 'key'")
-	}
-
-	lease, err := cli.Grant(ctx, 1)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Insert key with a lease of 1 second TTL
-	kv.Put(ctx, "key", "value", clientv3.WithLease(lease.ID))
-
-	gr, _ = kv.Get(ctx, "key")
-	if len(gr.Kvs) == 1 {
-		fmt.Println("Found 'key'")
-	}
-
-	// Let the TTL expire
-	time.Sleep(3 * time.Second)
-
-	gr, _ = kv.Get(ctx, "key")
-	if len(gr.Kvs) == 0 {
-		fmt.Println("No more 'key'")
-	}
-}
 
 func main() {
-	ctx, _ := context.WithTimeout(context.Background(), requestTimeout)
 	cli, err := clientv3.New(clientv3.Config{
+		Endpoints:   endpoints,
 		DialTimeout: dialTimeout,
-		Endpoints:   []string{"localhost:2379", "localhost:22379", "localhost:32379"},
 	})
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("Create Done!")
 	defer cli.Close()
 
-	kv := clientv3.NewKV(cli)
-
-	// GetSingleValueDemo(ctx, kv)
-	// GetMultipleValuesWithPaginationDemo(ctx, kv)
-	// WatchDemo(ctx, cli, kv)
-	// LeaseDemo(ctx, cli, kv)
-
-	// TestFuncPut(ctx, kv)
-	TestFuncGetRevision(ctx, kv)
-}
-
-func TestFuncGetRevision(ctx context.Context, kv clientv3.KV) {
-	revNum := 11
-
-	for i := revNum; i > 0; i-- {
-		fmt.Println("*** TestFunc()\n***Revision Number: ", i)
-
-		gr, err := kv.Get(ctx, "key1", clientv3.WithRev(int64(i)))
-		if err != nil {
-			// log.Fatal(err)
-			return
-		}
-
-		fmt.Println("Value: ", string(gr.Kvs[0].Value), "Revision: ", gr.Header.Revision)
-		fmt.Println("Create Rev: ", gr.Kvs[0].CreateRevision, "Mod Rev: ", gr.Kvs[0].ModRevision)
-		fmt.Println("Key Version: ", gr.Kvs[0].Version, "\n")
+	if _, err = cli.RoleAdd(context.TODO(), "root"); err != nil {
+		log.Fatal(err)
 	}
-}
-
-func TestFuncPut(ctx context.Context, kv clientv3.KV) {
-	pr, err := kv.Put(ctx, "key1", "444")
-	if err != nil {
+	if _, err = cli.UserAdd(context.TODO(), "root", "123"); err != nil {
+		log.Fatal(err)
+	}
+	if _, err = cli.UserGrantRole(context.TODO(), "root", "root"); err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println(pr.Header.Revision)
-}
-
-func CreateOne(ctx context.Context, kv clientv3.KV, key string, value string) (*clientv3.PutResponse, error) {
-	pr, err := kv.Put(ctx, "key1", "444")
-	if err != nil {
-		return nil, err
+	if _, err = cli.RoleAdd(context.TODO(), "r"); err != nil {
+		log.Fatal(err)
 	}
 
-	return pr, nil
-}
+	if _, err = cli.RoleGrantPermission(
+		context.TODO(),
+		"r",   // role name
+		"foo", // key
+		"zoo", // range end
+		clientv3.PermissionType(clientv3.PermReadWrite),
+	); err != nil {
+		log.Fatal(err)
+	}
+	if _, err = cli.UserAdd(context.TODO(), "u", "123"); err != nil {
+		log.Fatal(err)
+	}
+	if _, err = cli.UserGrantRole(context.TODO(), "u", "r"); err != nil {
+		log.Fatal(err)
+	}
+	if _, err = cli.AuthEnable(context.TODO()); err != nil {
+		log.Fatal(err)
+	}
 
-func DeleteOne(ctx context.Context, kv clientv3.KV, key string) {
-	kv.Delete(ctx, key)
+	cliAuth, err := clientv3.New(clientv3.Config{
+		Endpoints:   endpoints,
+		DialTimeout: dialTimeout,
+		Username:    "u",
+		Password:    "123",
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer cliAuth.Close()
 
-	return
-}
+	if _, err = cliAuth.Put(context.TODO(), "foo1", "bar"); err != nil {
+		log.Fatal(err)
+	}
 
-func DeleteMultiple(ctx context.Context, kv clientv3.KV, keyPrefix string) {
-	kv.Delete(ctx, keyPrefix, clientv3.WithPrefix())
+	_, err = cliAuth.Txn(context.TODO()).
+		If(clientv3.Compare(clientv3.Value("zoo1"), ">", "abc")).
+		Then(clientv3.OpPut("zoo1", "XYZ")).
+		Else(clientv3.OpPut("zoo1", "ABC")).
+		Commit()
+	fmt.Println(err)
 
-	return
+	// now check the permission with the root account
+	rootCli, err := clientv3.New(clientv3.Config{
+		Endpoints:   endpoints,
+		DialTimeout: dialTimeout,
+		Username:    "root",
+		Password:    "123",
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rootCli.Close()
+
+	resp, err := rootCli.RoleGet(context.TODO(), "r")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("user u permission: key %q, range end %q\n", resp.Perm[0].Key, resp.Perm[0].RangeEnd)
+
+	if _, err = rootCli.AuthDisable(context.TODO()); err != nil {
+		log.Fatal(err)
+	}
 }
